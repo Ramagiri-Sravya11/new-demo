5a0236e29a9e84ec522ea146b935951aff2f62b6
Ramagiri Sravya <57491439+Ramagiri-Sravya11@users.noreply.github.com>
2022-09-23 15:47:33 +0530

    Update Main2.java
    i made changes from sravya account

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -7,5 +7,6 @@ public class Main2 {
         // write your code here
         System.out.println("secong main ! ");
 // i made changes from local repo
+        // i made chanes from the remote respo from sravya account 
     }
 }
